59189c10a9cb6f032b5c07709a74fa630bd77d78
HendrikD2005 <144935017+HendrikD2005@users.noreply.github.com>
2025-09-07 21:22:45 +0200

    Removed unnecessary backend files; removed old frontend files

--- frontend/src/logic/main.js
+++
@@ -1,8 +0,0 @@
-import {app, BrowserWindow} from "electron"
-import path from "path"
-
-app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
-
-    mainWindow.loadFile(path.join(app.getPath(), "/dist-react/index.html"));
-})
